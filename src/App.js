eab802d903d28f617d0e450675a40fe5e7f8cfdf
Carlos Fernandez <carlosfdezb@gmail.com>
2021-05-04 00:38:19 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Suspense } from 'react';
-import { Router } from '@reach/router';
+import React from 'react';
 import GlobalStyles from './styles/GlobalStyles';
 
 //pages
@@ -7,12 +6,10 @@ import { Home } from './pages/Home';
 
 const App = () => {
   return (
-    <Suspense fallback={<div />}>
+    <>
       <GlobalStyles />
-      <Router>
-        <Home path='/' />
-      </Router>
-    </Suspense>
+      <Home />
+    </>
   );
 };
 
